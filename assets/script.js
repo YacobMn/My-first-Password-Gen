a94a3dc92c4cbe7b79058bc03b1274aa3965f764
unknown <yacobabr@gmail.com>
2023-10-03 21:03:01 -0500

    function added

--- assets/script.js
+++ assets/script.js
@@ -1,22 +1,41 @@
-// Assignment Code
+// Assignment Code 
 var generateBtn = document.querySelector("#generate");
 
-var numbersOfCharachters = "10"; 
-var includeUpperCase = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
-var includeNumbers = "123456789";
-var includeSpecialCharacters= "!@#$%&";
+// Global Variables 
+var numbersOfCharachters = "8 - 128";
+var UpperCase = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+var numbers = "123456789";
+var specialCharacters = "!@#$%&";
+var allCharacters = UpperCase + numbers + specialCharacters;
 
-function generatePassword(){
-  numbersOfCharachters = prompt("how many characters?")
-  includeUpperCase = confirm("do you want numbers?")
-  includeNumbers = prompt("do you want numbers")
-  includeSpecialCharacters = prompt("do you want special characters")
+
+
+function passWordLength() {
+  passWordLength = prompt("passWord length between 8-12 characters:")
+  if (passWordLength < 8 && passWordLength > 128) {
+    alert('you exceed the limmit number of characters')
+    return;
+  }
+}
+
+function generatePassword() {
+  passwordText = prompt("how many characters?")
 
 }
-  
+// options displey q
 
+function confirmUpperCase() {
+  UpperCasechoice = prompt('would you like to include lower case?');
+}
 
+function confirmLowerCase() {
+  lowerCase = prompt('would you like to inclde lowercase?');
 
+}
+
+function confirmNumbers() {
+  numberOption = prompt('would you like to inlcude number in your password?')
+}
 
 
 // Write password to the #password input
