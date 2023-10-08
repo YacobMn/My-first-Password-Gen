fbd04d7d7c32513c78a2bcf41b08be99e98f7826
unknown <yacobabr@gmail.com>
2023-10-08 13:13:47 -0500

    if conditions included

--- assets/script.js
+++ assets/script.js
@@ -3,71 +3,71 @@ var generateBtn = document.querySelector("#generate");
 
 // Global Variables 
 var numbersOfCharachters;
+var finalPassword= "";
+var limitPasswordLength;
+var numberEnter=12;
+var passwordLength;
 var upperCase;
-var numbers;
+var includeNumbers;
 var specialCharacters;
 var lowerCase;
-var finalPassword;
-var passWordLength;
-var allIncludecharacters;
+var randomNumber;
+var number;
 
 //characters arrays
-var specialCharacters = ['!', '@', '#', '$', '%', '&'];
-var numbers = ['1', '2', '3', '4', '5', '6', '7', '8', '9'];
-var upperCase = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z'];
-var lowerCase = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z'];
+var addSpecialCharacters = ['!', '@', '#', '$', '%', '&'];
+var numbersEnter = ['1', '2', '3', '4', '5', '6', '7', '8', '9'];
+var includeUpperCase = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z'];
+var includeLowerCase = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z'];
+var allChar = addSpecialCharacters + includeNumbers + upperCase + lowerCase;
+
+// randon number generator
+function randomNumber(){
+  var randomNumber = Math.floor(math.random() * 10);
+  return;
+}
 
 // If conditions
 
-passWordLength = prompt("whats the length of the password? must be (between 8 - 120) ")
-if (passWordLength >= 8 && passWordLength <= 128) {
-  inputValid = true;
-} else {
-  alert("passWord must be between 8-12 characters")
+function passWordLength(limitPasswordLength) {
+  limitPasswordLength = (passWordLength >= 12 && passWordLength >= 128);
+  return; 
+} if (numberEnter < 12 && numberEnter > 128) {
+  console.log("invalide Entery Number");
 }
-// what if the password is number in between the numbers?
-
-///................................................
-
-specialCharacters = prompt("you must include specail Characters")
-if (specialCharacters === ok){
-  inputInvalid = true;
-} else{
-  alert("you must include special Characters!")
+else if(numberEnter > 12 && numberEnter < 128) {
+  console.log("Good number choice!!");
 }
 
-specialCharacters = prompt("you must include numbers")
-if (specialCharacters === ok){
-  inputValid = true
-} else {
-  alert("numbers must be included")
+
+
+// password generator;
+function generatePassword(){
+  passwordLength = prompt("whats the password length?")
+  numberEnter = confirm("do you want to include numbers?")
+  addSpecialCharacters = confirm("do you want to include special characters?")
+  upperCase = confirm(" do you want to include uppercase?")
+  lowerCase = confirm(" do you want to include lowercase?")
+
+  var passwordLength = limitPasswordLength();
+  var upperCase = includeUpperCase();
+  var lowerCase = includeLowerCase();
+  var numbers = numberEnter();
+  
+  finalPassword =  includeLowerCase + includeNumbers + includeUpperCase + randomNumber;
+  finalPassword();
 }
 
-//loop
-
-function getARandomNumber() {
-  for (i = 0; i < numbersOfCharachters; i++) {
-    var index = Math.floor(math.random() * allIncludecharacters.length)
-    fainalpassword += allIncludecharacters[index];
-    console.log(passWordResult)
-  }
-}
-
-// final password num/
-function generatePassword() {
-  getARandomNumber();
-  getARandomNumber();
-  return finalPassword
-}
 
 // Write password to the #password input
 function writePassword() {
   var password = generatePassword();
   var passwordText = document.querySelector("#password");
-
-  passwordText.value = password;
+  passwordText.value = passowrd;
 
 }
 
+
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
+
